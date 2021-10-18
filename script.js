7eea571cf68fc2cc0cf2fb0b8e581a113f5e97ce
Augusto Manzoni Mariani <augusto.m26@aluno.ifsc.edu.br>
2021-10-18 13:27:49 -0300

    pontuação

--- script.js
+++ script.js
@@ -3,10 +3,31 @@
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 
-let activePlayer, playing;
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+let activePlayer, playing, scores, currentScore;
+
+const init = function () {
+    scores = [0, 0];
+    currentScore = 0;
+
+    
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+    
+  };
+  init();
+
+
 
 const switchPlayer = function () {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
     activePlayer = activePlayer === 0 ? 1 : 0;
     player0El.classList.toggle('player--active');
     player1El.classList.toggle('player--active');
