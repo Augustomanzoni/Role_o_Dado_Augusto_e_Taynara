42b0441a0497677c863b9f4917a7dbb139782000
Taynara-dev <taynara.mb@aluno.ifsc.edu.br>
2021-10-18 13:46:22 -0300

    dice el e roll

--- script.js
+++ script.js
@@ -8,18 +8,24 @@ const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
 
+const diceEl = document.querySelector('.dice');
+const btnRoll = document.querySelector('.btn--roll');
+
 let activePlayer, playing, scores, currentScore;
 
 const init = function () {
     scores = [0, 0];
     currentScore = 0;
+    activePlayer = 0;
+    playing = true;
 
-    
     score0El.textContent = 0;
     score1El.textContent = 0;
     current0El.textContent = 0;
     current1El.textContent = 0;
-    
+
+    diceEl.classList.add('hidden');
+
   };
   init();
 
@@ -32,3 +38,25 @@ const switchPlayer = function () {
     player0El.classList.toggle('player--active');
     player1El.classList.toggle('player--active');
   };
+
+  btnRoll.addEventListener('click', function () {
+    if (playing) {
+      
+      const dice = Math.trunc(Math.random() * 6) + 1;
+    
+      diceEl.classList.remove('hidden');
+      diceEl.src = `dice-${dice}.png`;
+    
+      if (dice !== 1) {
+        
+        currentScore += dice;
+        document.getElementById(
+          `current--${activePlayer}`
+        ).textContent = currentScore;
+
+      } else {
+        switchPlayer();
+      }
+    }
+  });
+
