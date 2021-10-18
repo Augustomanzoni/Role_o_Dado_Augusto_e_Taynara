bdbc4605dc414629cdb6eba22a3a0c1b5248c976
Taynara-dev <taynara.mb@aluno.ifsc.edu.br>
2021-10-18 11:45:30 -0300

    players

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
+let activePlayer, playing;
+
+const switchPlayer = function () {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+  };
